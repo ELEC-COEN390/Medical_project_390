9e037b4510b37f4c16a74402e0c5cab0ceb2e651
antishr <antish.kevin@gmail.com>
2025-04-04 17:36:20 -0400

    Doctor dashboard implementation

--- app/src/main/java/com/example/moodproject/DoctorDashboard.java
+++ app/src/main/java/com/example/moodproject/DoctorDashboard.java
@@ -1,5 +1,6 @@
 package com.example.moodproject;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,6 +37,7 @@ public class DoctorDashboard extends AppCompatActivity
     RecyclerView recyclerViewPatients;
     FloatingActionButton fabAddPatient;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
